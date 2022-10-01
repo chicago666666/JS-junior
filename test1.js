5e962e29d73992e206aeff3e8171653f0863ad12
Ruslan <chicago666666@gmail.com>
2022-10-01 14:25:12 +0300

    1 Переменные

---
+++ test1.js
@@ -0,0 +1,11 @@
+//Переменные
+const FirstName = "Ruslan";
+const LastName = "Horbunov";
+let age = 34;
+const isNotValid = false
+const isValid = true
+const _name = ''
+const $ = 250
+
+//Операторы
+alert('My name: ' + FirstName)
